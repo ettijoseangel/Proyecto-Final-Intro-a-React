5f09a81ec4128743a02e953ce7fe15d397daf8ad
Angel <etti.joseangel@gmail.com>
2026-07-20 22:54:07 -0600

    Configuracion de estado de autenticacion global, persistencia y proteccion de rutas

--- mi-twitter-clon/src/App.jsx
+++ mi-twitter-clon/src/App.jsx
@@ -1,14 +1,47 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { useState } from "react";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 const App = () => {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  // Funcion para iniciar sesion
+  const login = (username) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  // Funcion para cerrar sesion
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        {/* Ruta publica para el Login */}
+        <Route path="/login" element={<Login onLogin={login} />} />
+
+        {/*Ruta Home: Le pasamos el usuario actual y la funcion para salir*/}
+        <Route path="/" element={<Home user={user} logout={logout} />} />
+
+        {/*Ruta protegida. Si no hay usuario, lo patea al /login */}
+        <Route
+          path="/profile"
+          element={user ? <Profile user={user} /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
